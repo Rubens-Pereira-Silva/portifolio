db496864e1969453715d43bb82e3e40e22a8f2b0
Rubens Pereira da Silva <rubenspereira1029@gmail.com>
2025-06-23 11:00:23 -0300

    #

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,15 @@
 let theme = "dark"
+let imgTheme = document.getElementById("imgTheme")
 function switchTheme(){
     body = document.body
     if(theme == "dark"){
         body.setAttribute("data-theme", "light")
         theme = "light"
+        imgTheme.src = "/src/assets/header/sun.png"
     }
     else{
         body.setAttribute("data-theme", "dark")
         theme = "dark"
+        imgTheme.src = "/src/assets/header/moon.png"
     }
 }
